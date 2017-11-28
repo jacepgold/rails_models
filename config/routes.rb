cec19eba2d088eb89e0c40438d1eb33640e4824c
Jace P. Gold <Jace@JaceGold.com>
2017-11-28 16:38:04 -0700

    Create Routes and persons controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'persons/index'
+
+  # get 'persons/show'
+
+  # get 'persons/new'
+
+  resources :persons
+  root 'persons#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
