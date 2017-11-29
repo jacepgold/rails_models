40cbcb81416c4259c8282e035f11c60fa9353f3c
Jace P. Gold <Jace@JaceGold.com>
2017-11-28 17:08:47 -0700

    Create Index and Edit Person Pages

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,8 +1,14 @@
 class PersonsController < ApplicationController
   def index
+    @Persons = Person.all
   end
 
   def show
+    @Person = Person.find(params[:id])
+  end
+
+  def edit
+    @Person = Person.find(params[:id])
   end
 
   def new
